7dd54330d4a9e424c1b6770cec3c29c239ce12e0
jaimitra <jaimitra.d@techouts.co.in>
2026-02-14 15:19:20 +0530

    We can't play

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
 city = input("Enter city: ")
-weather = f'Weather is nice in {city} it\'s time to play.'
+weather = f'Weather is not nice in {city} we can\'t play everyone.'
+print("HI")
 print(weather)
